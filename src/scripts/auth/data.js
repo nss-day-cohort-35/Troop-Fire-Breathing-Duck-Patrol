20053010bd387b2e9b6e4596771b7a64466ed163
Phil <Griswold@Phils-MacBook-Pro.local>
2019-09-24 14:51:07 -0500

    got buttons working

--- src/scripts/auth/data.js
+++ src/scripts/auth/data.js
@@ -1,20 +1,26 @@
 const API = {
 
-getUserInfo: () => {
-    return fetch("http://localhost:8088/users")
-        .then(response => response.json())
+    getUserInfo: () => {
+        return fetch("http://localhost:8088/users")
+            .then(res => res.json())
 
-},
+    },
 
-saveUserInfo: user => {
-    return fetch("http://localhost:8088/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
-    })
-}
+    getSingleUsersInfo: (email, password) => {
+        return fetch(`http://localhost:8088/users?_userEmail=${email}&&userPassword=${password}`)
+            .then(res => res.json())
+
+    },
+
+    saveUserInfo: user => {
+        return fetch("http://localhost:8088/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+    }
 }
 
 //use fetch to create journal entry
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,23 @@ import DOM from "./auth/userDOM.js"
 import API from "./auth/data.js"
 import allFunctions from "./factory.js"
 
-const dashboardDOM = ()=> {
+const dashboardDOM = () => {
     let mainContainer = document.querySelector("#container")
     mainContainer.innerHTML = allFunctions.dashboard();
 }
+
 // checks to see if sessionStorage is empty.
-if  (sessionStorage.getItem("user") === null) {
-DOM.renderLandingPage();
+if (sessionStorage.getItem("user") === null) {
+    DOM.renderLandingPage();
 } else {
-     dashboardDOM();
-  }
-
-
+    dashboardDOM();
+    document.getElementById("eventPageContainer").style.visibility = "hidden";
+    document.querySelector("#createEventButton").addEventListener("click", () => {
+        API.saveUserInfo().then(res => res.json()).then(res => {
+            sessionStorage.setItem("user", res.id)
+                // const getUserId = sessionStorage.getItem("user")
+            let mainContainer = document.querySelector("#container")
+            mainContainer.innerHTML += document.querySelector("#eventPageContainer")
+        })
+    })
+}
